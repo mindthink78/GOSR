dd7d172d5533b9c655f2ffe1745f86272ee41033
Danya <mindthink@MacBook-Air-Danya.local>
2026-06-12 00:55:17 +0300

    done

--- http/httpQuery.go
+++ http/httpQuery.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func queryHandler(w http.ResponseWriter, r *http.Request) {
+	firstParam := r.URL.Query().Get("first")
+	secondParam := r.URL.Query().Get("second")
+
+	fmt.Println("first:", firstParam)
+	fmt.Println("second:", secondParam)
+}
+
+func main() {
+	http.HandleFunc("/query", queryHandler)
+
+	fmt.Println("SERVER STARTED")
+
+	if err := http.ListenAndServe(":9091", nil); err != nil {
+		fmt.Println("Server crashed")
+		return
+	}
+}
